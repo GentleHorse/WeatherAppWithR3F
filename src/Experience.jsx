58e1811f234ec5c68a4a09ed28f74a916c4761d1
Toshi E <anch.abc.t@gmail.com>
2023-12-09 17:16:34 +0100

    Switch smaller weather icon models for reducing calls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,13 +1,7 @@
-import { OrbitControls } from "@react-three/drei";
+import { useGLTF, OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import Floor from "./components/Floor.jsx";
-import Clear from "./components/models/Clear.jsx";
-import Cloud from "./components/models/Cloud.jsx";
-import Drizzle from "./components/models/Drizzle.jsx";
-import Mist from "./components/models/Mist.jsx";
-import Rain from "./components/models/Rain.jsx";
-import Snow from "./components/models/Snow.jsx";
-import Thunderstorm from "./components/models/Thunderstorm.jsx";
+import WeatherIcons from "./components/models/WeatherIcons.jsx";
 
 export default function Experience() {
   return (
@@ -21,13 +15,7 @@ export default function Experience() {
 
       <Floor />
 
-        <Clear scale={0.2} position={[-9, 0, 1.5]} />
-        <Cloud scale={0.2} position={[-6, 0, 1.5]} />
-        <Drizzle scale={0.2} position={[-3, 0, 1.5]} />
-        <Mist scale={0.2} position={[0, 0, 1.5]} />
-        <Rain scale={0.2} position={[3, 0, 1.5]} />
-        <Snow scale={0.2} position={[6, 0, 1.5]} />
-        <Thunderstorm scale={0.2} position={[9, 0, 1.5]} />
+      <WeatherIcons />
     </>
   );
 }
--- src/components/models/WeatherIcons.jsx
+++ src/components/models/WeatherIcons.jsx
@@ -1,24 +1,24 @@
 import { useGLTF } from "@react-three/drei";
 
 const WeatherIcons = () => {
-  const clear = useGLTF("./models/weather-icons/clear.glb");
-  const cloud = useGLTF("./models/weather-icons/cloud.glb");
-  const drizzle = useGLTF("./models/weather-icons/drizzle.glb");
-  const mist = useGLTF("./models/weather-icons/mist.glb");
-  const rain = useGLTF("./models/weather-icons/rain.glb");
-  const snow = useGLTF("./models/weather-icons/snow.glb");
-  const thunderstorm = useGLTF("./models/weather-icons/thunderstorm.glb");
+  const clear = useGLTF("./models/weather-icons/clear-small.glb");
+  const cloud = useGLTF("./models/weather-icons/cloud-small.glb");
+  const drizzle = useGLTF("./models/weather-icons/drizzle-small.glb");
+  const mist = useGLTF("./models/weather-icons/mist-small.glb");
+  const rain = useGLTF("./models/weather-icons/rain-small.glb");
+  const snow = useGLTF("./models/weather-icons/snow-small.glb");
+  const thunderstorm = useGLTF("./models/weather-icons/thunderstorm-small.glb");
 
   return (
-    <group position={[0, 3, -3]}>
-      <primitive object={clear.scene} scale={0.2} position-x={-6} />
-      <primitive object={cloud.scene} scale={0.2} position-x={-4} />
-      <primitive object={drizzle.scene} scale={0.2} position-x={-2} />
-      <primitive object={mist.scene} scale={0.2} position-x={0} />
-      <primitive object={rain.scene} scale={0.2} position-x={2} />
-      <primitive object={snow.scene} scale={0.2} position-x={4} />
-      <primitive object={thunderstorm.scene} scale={0.2} position-x={6} />
-    </group>
+    <group position={[0, 0.5, 0]}>
+    <primitive object={clear.scene} scale={1} position-x={-6} />
+    <primitive object={cloud.scene} scale={1} position-x={-4} />
+    <primitive object={drizzle.scene} scale={1} position-x={-2} />
+    <primitive object={mist.scene} scale={1} position-x={0} />
+    <primitive object={rain.scene} scale={1} position-x={2} />
+    <primitive object={snow.scene} scale={1} position-x={4} />
+    <primitive object={thunderstorm.scene} scale={1} position-x={6} />
+  </group>
   );
 };
 
